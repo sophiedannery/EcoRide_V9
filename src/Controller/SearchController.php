25e632a05ae1de6ac1d8ec0d42b9fc4ec509f1ce
sophiedannery <dannery.sophie@gmail.com>
2025-05-19 14:33:29 +0200

    feat(recherche_trajet): ajout du formulaire de recherche de trajet et logique métier

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,71 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\TrajetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\VarDumper\VarDumper;
 
 final class SearchController extends AbstractController
 {
     #[Route('/search', name: 'app_search')]
-    public function search(Request $request): Response
+    public function search(Request $request, TrajetRepository $repo): Response
     {
 
-        $depart = $request->query->get('depart');
-        $arrivee = $request->query->get('arrivee');
-        $date = $request->query->get('date');
+        $from = $request->query->get('depart', '');
+        $to = $request->query->get('arrivee', '');
+        $date = new \DateTime($request->query->get('date', 'now'));
 
-        $allrides = [
-            [
-                'id' => 1,
-                'driver' => 'Laura',
-                'vehicle' => 'Tesla',
-                'price' => 18,
-                'depart' => 'Paris',
-                'arrivee' => 'Lyon',
-                'date' => '2025-06-01',
-            ],
-            [
-                'id' => 2,
-                'driver' => 'Luca',
-                'vehicle' => 'Ferrari',
-                'price' => 23,
-                'depart' => 'Marseille',
-                'arrivee' => 'Nice',
-                'date' => '2025-06-01',
-            ],
-            [
-                'id' => 3,
-                'driver' => 'Victoria',
-                'vehicle' => 'Petit Poney',
-                'price' => 7,
-                'depart' => 'Paris',
-                'arrivee' => 'Lyon',
-                'date' => '2025-06-02',
-            ],
-            [
-                'id' => 4,
-                'driver' => 'Nine',
-                'vehicle' => 'Kinderkraft',
-                'price' => 4,
-                'depart' => 'Paris',
-                'arrivee' => 'Lyon',
-                'date' => '2025-06-01',
-            ],
+        VarDumper::dump([
+            'from' => $from,
+            'to' => $to,
+            'date' => $date->format('Y-m-d'),
+        ]);
 
-        ];
-
-        $rides = array_filter($allrides, function (array $ride) use ($depart, $arrivee, $date) {
-            return $ride['depart'] === $depart && $ride['arrivee'] === $arrivee && $ride['date'] === $date;
-        });
-
+        $trajets = $repo->searchTrips($from, $to, $date);
 
         return $this->render('search/results.html.twig', [
-            'depart' => $depart,
-            'arrivee' => $arrivee,
-            'date' => $date,
-            'rides' => $rides,
+            'trajets' => $trajets,
         ]);
     }
 }
--- src/Repository/TrajetRepository.php
+++ src/Repository/TrajetRepository.php
@@ -16,6 +16,38 @@ class TrajetRepository extends ServiceEntityRepository
         parent::__construct($registry, Trajet::class);
     }
 
+
+    public function searchTrips(string $from, string $to, \DateTimeInterface $date): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = <<<SQL
+
+        SELECT
+            t.id_trajet, 
+            t.adresse_depart, 
+            t.adresse_arrivee, 
+            DATE_FORMAT(t.date_depart, '%Y-%m-%d %H:%i') AS date_depart, 
+            t.prix,
+            t.places_restantes, 
+            u.pseudo AS chauffeur
+        FROM trajet AS t
+        JOIN utilisateur u ON t.chauffeur_id = u.id_utilisateur
+        WHERE 
+            t.adresse_depart = ?
+            AND t.adresse_arrivee = ?
+            AND DATE(t.date_depart) = ?
+            AND t.places_restantes > 0
+        ORDER BY t.date_depart
+        SQL;
+
+        return $conn->executeQuery($sql, [
+            $from,
+            $to,
+            $date->format('Y-m-d'),
+        ])->fetchAllAssociative();
+    }
+
     //    /**
     //     * @return Trajet[] Returns an array of Trajet objects
     //     */
