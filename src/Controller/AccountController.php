a2821cc743b334b2d887b7feae762332c2ec343d
sophiedannery <dannery.sophie@gmail.com>
2025-05-27 15:29:55 +0200

    feat(espace_user): annulation changement statut

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -17,30 +17,8 @@ final class AccountController extends AbstractController
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
 
-        if (in_array($user->getStatut(), ['chauffeur', 'mixte'], true) && $user->getVehicules()->isEmpty()) {
-            return $this->redirectToRoute('app_vehicule_new');
-        }
-
-        if (in_array($user->getStatut(), ['chauffeur', 'mixte'], true) && $user->getPreference()->isEmpty()) {
-            return $this->redirectToRoute('app_preference_new');
-        }
-
         return $this->render('account/index.html.twig', [
             'user' => $user,
         ]);
-    }
-
-
-    #[Route('/account/become-chauffeur', name: 'app_account_become_chauffeur')]
-    public function becomeChauffeur(EntityManagerInterface $em): Response
-    {
-        /** @var \App\Entity\User $user */
-        $user = $this->getUser();
-
-        $nouveauStatut = $user->getStatut() === 'passager' ? 'mixte' : 'chauffeur';
-        $user->setStatut($nouveauStatut);
-        $em->flush();
-
-        return $this->redirectToRoute('app_account');
     }
 }
