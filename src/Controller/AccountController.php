001cb2a6e8965b7473e8251574dc7353548b3535
sophiedannery <dannery.sophie@gmail.com>
2025-05-27 13:16:05 +0200

    feat(espace_user): ajout page mon espage

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AccountController extends AbstractController
+{
+    #[Route('/account', name: 'app_account')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('account/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
